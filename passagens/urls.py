8795946e95c2f6031b241cd988f5a2f08cb5d638
harrison <harrisonpinchieri@gmail.com>
2023-09-01 01:28:31 -0300

    v0.2

--- passagens/urls.py
+++ passagens/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import index, revisao_consulta
 
 urlpatterns = [
-    path('',index, name='index'),
-    path('minha_consulta',revisao_consulta,name='minha_consulta')
+    path("", index, name="index"),
+    path("minha_consulta", revisao_consulta, name="minha_consulta"),
 ]
--- passagens/views.py
+++ passagens/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from passagens.forms import PassagemForms
 
+
 # Create your views here.
 def index(request):
     form = PassagemForms()
-    contexto = {'form':form}
-    return render(request,'passagens/index.html',contexto)
+    contexto = {"form": form}
+    return render(request, "passagens/index.html", contexto)
 
 
 def revisao_consulta(request):
-    if request.method == 'POST':
-        form = PassagemForms(request.POST)        
-    contexto = {'form':form}
-    return render(request,'minha_consulta/index.html',contexto)    
+    if request.method == "POST":
+        form = PassagemForms(request.POST)
+    contexto = {"form": form}
+    return render(request, "passagens/minha_consulta.html", contexto)
